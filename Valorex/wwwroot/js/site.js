e0db90e51d92d7b88267da029326a7639b1737cb
BrunoSantosOliveira27 <bruunoosanntos@outlook.com>
2024-09-20 22:21:48 -0300

    Concluido os Cards com Todas as Imagens

--- Valorex/wwwroot/js/site.js
+++ Valorex/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function filter(categoria){
+    //alterar a visibilidade dos cards
+    let cardCount = 0;
+    document.querySelectorAll('.poke').forEach(card => {
+        card.style.display = "flex";
+        cardCount +=1;
+        if(!card.classList.contains(categoria) && categoria !== ''){ 
+            card.style.display = 'none';
+            cardCount -=1;
+    }});
 
-// Write your JavaScript code.
+    // verificar se existem cards 
+   let zeroPersonagem = document.querySelector('#zeroPersonagem');
+   cardCount == 0 ? 
+   zeroPersonagem.classList.remove('d-none'): 
+   zeroPersonagem.classList.add('d-none');
+
+   // alterar o estilo dos botoes de filtro
+   document.querySelectorAll('.btn-filter').forEach(button =>{
+    button.classList.add('btn-sm');
+    button.classList.remove('btn-md');
+    if(button.id == `btn-${categoria}`){
+        button.classList.remove('btn-sm');
+    button.classList.add('btn-md');
+    } 
+   });
+
+}
